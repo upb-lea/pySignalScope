ea279445f20cda63ef2a03ea172db2597e21920b
gituser789 <62549000+gituser789@users.noreply.github.com>
2024-11-05 12:57:37 +0100

    add impedance feature

---
+++ examples/impedance_example.py
@@ -0,0 +1,22 @@
+"""Example how to generate, modify and plot impedance objects."""
+import pysignalscope as pss
+
+# import data as a channel object
+example_data_0mm5 = pss.HandleImpedance.from_waynekerr('impedance_example_data_0mm5.csv', '0.5mm air gap')
+example_data_1mm5 = pss.HandleImpedance.from_waynekerr('impedance_example_data1mm5.csv', '1.5mm air gap')
+example_data_rlc = pss.HandleImpedance.from_rlc('l', 1000, 500e-6, 10e-12)
+
+# modify the data, e.g. change the color.
+# For color changes, you can use standard colors or colors from the leapythontoolbox, see this example
+example_data_0mm5 = pss.HandleImpedance.modify(example_data_0mm5, channel_color="red", channel_unit="random unit")
+example_data_1mm5 = pss.HandleImpedance.modify(example_data_1mm5, channel_color=pss.gnome_colors["blue"])
+example_data_rlc = pss.HandleImpedance.modify(example_data_rlc, channel_color=pss.gnome_colors["green"], channel_label="from rlc")
+
+# # recalculate rlc data from a impedance curve
+recalculated_r, recalculated_l, recalculated_c = (pss.HandleImpedance.calc_rlc(example_data_rlc, 'l', f_calc_l=10e3, f_calc_c=10e7, plot_figure=True))
+print(f"{recalculated_r=}")
+print(f"{recalculated_l=}")
+print(f"{recalculated_c=}")
+
+# plot multiple channel data
+pss.HandleImpedance.plot_impedance([example_data_0mm5, example_data_1mm5, example_data_rlc])
