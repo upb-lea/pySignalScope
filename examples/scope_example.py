94b91aa16ec9d25d0611f8b28d2e55398ec23686
gituser789 <62549000+gituser789@users.noreply.github.com>
2024-11-06 13:57:07 +0100

    change scope classmethods to staticmethods

--- examples/scope_example.py
+++ examples/scope_example.py
@@ -9,75 +9,75 @@ import pysignalscope as pss
 # ------------------------------------------
 
 # Read curves from scope csv file
-[voltage_prim, voltage_sec, current_prim, current_sec] = pss.Scope.from_tektronix('scope_example_data_tek.csv')
+[voltage_prim, voltage_sec, current_prim, current_sec] = pss.HandleScope.from_tektronix('scope_example_data_tek.csv')
 
 # Add labels and units to channels
-voltage_prim.modify(channel_label='voltage primary', channel_unit='V', )
-voltage_sec.modify(channel_label='voltage secondary', channel_unit='V')
-current_prim.modify(channel_label='current primary', channel_unit='A')
-current_sec.modify(channel_label='current secondary', channel_unit='A')
+voltage_prim = pss.HandleScope.modify(voltage_prim, channel_label='voltage primary', channel_unit='V', )
+voltage_sec = pss.HandleScope.modify(voltage_sec, channel_label='voltage secondary', channel_unit='V')
+current_prim = pss.HandleScope.modify(current_prim, channel_label='current primary', channel_unit='A')
+current_sec = pss.HandleScope.modify(current_sec, channel_label='Secondary current', channel_unit='A')
 
 # Show gain and DC-offset
-current_sec.modify(channel_data_factor=1.3, channel_data_offset=10)
+current_sec = pss.HandleScope.modify(current_sec, channel_data_factor=1.3, channel_data_offset=10)
 
 # Plot channels
-fig1 = pss.Scope.plot_channels([voltage_prim, voltage_sec], [current_prim, current_sec], timebase='us')
-pss.Scope.save(fig1, 'test')
+fig1 = pss.HandleScope.plot_channels([voltage_prim, voltage_sec], [current_prim, current_sec], timebase='us')
+pss.HandleScope.save(fig1, 'test')
 
 # short channels to a single period, perform FFT for current waveforms
-current_prim.short_to_period(f0=200000)
-current_prim.modify(channel_time_shift=5e-6)
-current_prim.fft()
+current_prim = pss.HandleScope.short_to_period(current_prim, f0=200000)
+current_prim = pss.HandleScope.modify(current_prim, channel_time_shift=5e-6)
+# pss.HandleScope.fft(current_prim)
 
 # ------------------------------------------
 # Example 2: Read curves from LeCroy csv-Files and GeckoCirucits. Compare these signals.
 # ------------------------------------------
 
-meas_v_ob, meas_il_ib, meas_il_ob = pss.Scope.from_lecroy('scope_example_data_lecroy_1.csv',
-                                                          'scope_example_data_lecroy_2.csv',
-                                                          'scope_example_data_lecroy_3.csv')
+meas_v_ob, meas_il_ib, meas_il_ob = pss.HandleScope.from_lecroy('scope_example_data_lecroy_1.csv',
+                                                                'scope_example_data_lecroy_2.csv',
+                                                                'scope_example_data_lecroy_3.csv')
 
-meas_v_ob.modify(channel_label='vl_ob_meas', channel_unit='V')
-meas_il_ib.modify(channel_label='il_ib_meas', channel_unit='A', channel_color='b')
-meas_il_ob.modify(channel_label='il_ob_meas', channel_unit='A', channel_color='m', channel_data_offset=1.5)
+meas_v_ob = pss.HandleScope.modify(meas_v_ob, channel_label='vl_ob_meas', channel_unit='V')
+meas_il_ib = pss.HandleScope.modify(meas_il_ib, channel_label='il_ib_meas', channel_unit='A', channel_color='b')
+meas_il_ob = pss.HandleScope.modify(meas_il_ob, channel_label='il_ob_meas', channel_unit='A', channel_color='m', channel_data_offset=1.5)
 
 # read dataset from gecko simulation
-gecko_data = pss.Scope.from_geckocircuits('scope_example_data_gecko', f0=200000)
+gecko_data = pss.HandleScope.from_geckocircuits('scope_example_data_gecko', f0=200000)
 
 gecko_il_ib = gecko_data[0]
-gecko_il_ib.modify(channel_label='il_ib_gecko', channel_unit='A', channel_color='r', channel_linestyle="--")
+gecko_il_ib = pss.HandleScope.modify(gecko_il_ib, channel_label='il_ib_gecko', channel_unit='A', channel_color='r', channel_linestyle="--")
 gecko_il_ob = gecko_data[-1]
-gecko_il_ob.modify(channel_data_factor=-1, channel_label='il_ob_gecko', channel_unit='A', channel_color='g')
+gecko_il_ob = pss.HandleScope.modify(gecko_il_ob, channel_data_factor=-1, channel_label='il_ob_gecko', channel_unit='A', channel_color='g')
 
 # compare both waveforms
-pss.Scope.compare_channels(meas_il_ib, gecko_il_ib, meas_il_ob, gecko_il_ob, shift=[-67.53e-6, 0, -67.53e-6, 0], timebase='us')
+# pss.HandleScope.compare_channels(meas_il_ib, gecko_il_ib, meas_il_ob, gecko_il_ob, shift=[-67.53e-6, 0, -67.53e-6, 0], timebase='us')
 
 # Shift data
-meas_il_ob.modify(channel_time_shift=-67.53e-6)
+meas_il_ob = pss.HandleScope.modify(meas_il_ob, channel_time_shift=-67.53e-6)
 # Calculate sum
-sum_il_ob = pss.Scope.add(meas_il_ob, meas_il_ob)
+sum_il_ob = pss.HandleScope.add(meas_il_ob, meas_il_ob)
 
 # Calculate difference
-diff_il_ob = pss.Scope.subtract(sum_il_ob, meas_il_ob)
+diff_il_ob = pss.HandleScope.subtract(sum_il_ob, meas_il_ob)
 
 # Copy values to new variable
-abs_diff_il_ob = diff_il_ob.copy()
+abs_diff_il_ob = pss.HandleScope.copy(diff_il_ob)
 # Calculate absolute values
-abs_diff_il_ob.abs()
+pss.HandleScope.abs(abs_diff_il_ob)
 # Copy values to new variable
-sqr_diff_il_ob = diff_il_ob.copy()
+sqr_diff_il_ob = pss.HandleScope.copy(diff_il_ob)
 # Calculate square
-sqr_diff_il_ob.square()
+pss.HandleScope.square(sqr_diff_il_ob)
 # Calculate values:
 # Root means square
-rms_diff_il_ob = diff_il_ob.rms()
+rms_diff_il_ob = pss.HandleScope.rms(diff_il_ob)
 # Average value
-mean_diff_il_ob = diff_il_ob.mean()
+mean_diff_il_ob = pss.HandleScope.mean(diff_il_ob)
 # Average of absolute values
-absmean_diff_il_ob = diff_il_ob.absmean()
+absmean_diff_il_ob = pss.HandleScope.absmean(diff_il_ob)
 # Print calculated values
 print(f"Rootmeansquare={rms_diff_il_ob}\nAverage value={mean_diff_il_ob}\nAbsolute average value={absmean_diff_il_ob}\n")
 # Plot results
-pss.Scope.plot_channels([diff_il_ob])
+pss.HandleScope.plot_channels([diff_il_ob])
 # Plot results
-pss.Scope.plot_channels([diff_il_ob, abs_diff_il_ob, sqr_diff_il_ob])
+pss.HandleScope.plot_channels([diff_il_ob, abs_diff_il_ob, sqr_diff_il_ob])
---
+++ pysignalscope/scope_dataclass.py
@@ -0,0 +1,24 @@
+"""Definition of the scope dataclass."""
+
+# python libraries
+import dataclasses
+from typing import Optional
+
+# 3rd party libraries
+import numpy as np
+
+@dataclasses.dataclass
+class Scope:
+    # mandatory measurement data
+    channel_time: np.array
+    channel_data: np.array
+
+    # optional data
+    channel_label: Optional[str]
+    channel_unit: Optional[str]
+    channel_color: Optional[str]
+    channel_source: Optional[str]
+    channel_linestyle: Optional[str]
+
+    # meta data
+    modulename: Optional[str]
