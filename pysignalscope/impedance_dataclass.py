60f85677ca343cae68ff1af625437e418bd82e9a
gituser789 <62549000+gituser789@users.noreply.github.com>
2024-12-03 16:40:35 +0100

    add keys for documentation

--- pysignalscope/impedance_dataclass.py
+++ pysignalscope/impedance_dataclass.py
@@ -11,11 +11,17 @@ import numpy as np
 class Impedance:
     """Dataclass to share scope figures in a special format, to keep labels, units and voltages belonging to a certain curve."""
 
-    channel_frequency: np.array
-    channel_impedance: np.array
-    channel_phase: np.array
-    channel_label: Union[str]
-    channel_unit: Union[str]
-    channel_color: Union[str]
-    channel_source: Union[str]
-    channel_linestyle: Union[str]
+    # mandatory data
+    channel_frequency: np.array  #: mandatory: frequency data (mandatory)
+    channel_impedance: np.array  #: mandatory: impedance data (mandatory)
+    channel_phase: np.array  #: mandatory: phase data (mandatory)
+
+    # optional data
+    channel_label: Union[str]  #: channel label displayed in a plot (optional)
+    channel_unit: Union[str]  #: channel unit displayed in a plot (optional)
+    channel_color: Union[str]  #: channel color displayed in a plot (optional)
+    channel_linestyle: Union[str]  #: channel linestyle displayed in a plot (optional)
+
+    # meta data
+    channel_source: Union[str]  #: channel source, additional meta data (optional)
+
--- pysignalscope/scope_dataclass.py
+++ pysignalscope/scope_dataclass.py
@@ -9,18 +9,18 @@ import numpy as np
 
 @dataclasses.dataclass
 class Scope:
-    """Scope data class."""
+    """Scope data class. Attributes as the following:"""
 
     # mandatory measurement data
-    channel_time: np.array
-    channel_data: np.array
+    channel_time: np.array  #: time series of the channel (mandatory)
+    channel_data: np.array  #: data series of the channel (mandatory)
 
     # optional data
-    channel_label: Optional[str]
-    channel_unit: Optional[str]
-    channel_color: Optional[str]
-    channel_source: Optional[str]
-    channel_linestyle: Optional[str]
+    channel_label: Optional[str]  #: channel label displayed in a plot (optional)
+    channel_unit: Optional[str]  #: channel unit displayed in a plot (optional)
+    channel_color: Optional[str]  #: channel color in a plot (optional)
+    channel_linestyle: Optional[str]  #: channel linestyle in a plot (optional)
 
     # meta data
+    channel_source: Optional[str]  #: channel source, additional meta data (optional)
     modulename: Optional[str]
